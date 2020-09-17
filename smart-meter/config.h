572d1aa2428a6bb62bc7e275019b9da9d94d58e6
amadeuspzs <amadeus.stevenson@gmail.com>
2020-09-17 06:44:16 +0100

    Add MQTT topics

--- smart-meter/config.h
+++ smart-meter/config.h
@@ -10,8 +10,9 @@ IPAddress subnet(255,255,255,0);
 
 // Sensor configuration
 #define optical D1
-#define ppwh 1/3.2
+#define ppwh 1/3.2 //3200 pulses/kWh = 1/3.2 pulse per Wh
 
 // Reporting
 #define mqtt_server "0.0.0.0"
-
+#define power_topic device "/power"
+#define energy_topic device "/energy"
--- smart-meter/smart-meter.ino
+++ smart-meter/smart-meter.ino
@@ -7,8 +7,13 @@
  *
  */
 
-// where is the sensor connected?
-int optical = D1;
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include "config.h"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 //Number of pulses, used to measure energy.
 long pulseCount = 0;
@@ -19,12 +24,35 @@ unsigned long pulseTime,lastTime;
 //power and energy
 double power, elapsedkWh, T;
 
-//Number of pulses per Wh - found or set on the meter.
-double ppwh = 1/3.2; //3200 pulses/kWh = 1/3.2 pulse per Wh
-
 void setup()
 {
     Serial.begin(115200);
+    while (!Serial) {}
+
+    Serial.setDebugOutput(true);
+
+    Serial.print("Welcome ");
+    Serial.println(device);
+    Serial.print("Connecting to ");
+    Serial.println(wifi_ssid);
+    
+    // switch on WiFi
+    WiFi.hostname(wifi_hostname);
+    WiFi.begin(wifi_ssid, wifi_password);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(100);
+      Serial.print(".");
+    }
+    
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    // configure MQTT server
+    client.setServer(mqtt_server, 1883);
+
     // KWH interrupt attached to IRQ 1 = D1 = GPIO5
     attachInterrupt(digitalPinToInterrupt(optical), onPulse, FALLING);
 }
@@ -57,4 +85,24 @@ ICACHE_RAM_ATTR void onPulse()
     Serial.print(" kW ");
     Serial.print(elapsedkWh,3);
     Serial.println(" kWh");
+
+    // connect to MQTT server
+    if (!client.connected()) {
+      Serial.print("Attempting MQTT connection...");
+      if (client.connect("ESP8266Client")) {
+        Serial.println("connected");
+      } else {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again in 1 second");
+        // Wait 1 second before retrying
+        delay(1000);
+      } //end if client.connect
+    } // end if client.connected
+
+    Serial.println("Publishing to MQTT...");
+  
+    // publish new reading
+    client.publish(power_topic, String(power/1000.0).c_str(), true);
+    client.publish(energy_topic, String(elapsedkWh).c_str(), true);
 }
