527d7f174127b735e8e82d7257be2e055f0f53db
amadeuspzs <amadeus.stevenson@gmail.com>
2020-09-17 06:44:43 +0100

    Remove static routing

--- smart-meter/config.h
+++ smart-meter/config.h
@@ -4,9 +4,6 @@
 #define wifi_hostname device
 #define wifi_ssid "SSID"
 #define wifi_password "PASSPHRASE"
-IPAddress ip(0,0,0,0);
-IPAddress gateway(0,0,0,0);
-IPAddress subnet(255,255,255,0);
 
 // Sensor configuration
 #define optical D1
