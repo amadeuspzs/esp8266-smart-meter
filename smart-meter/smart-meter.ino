6486c887b0cdd6c13d2f4c2b76423257c54a0ff5
amadeuspzs <amadeus.stevenson@gmail.com>
2020-09-19 12:30:05 +0100

    Add ct code:

--- smart-meter/smart-meter.ino
+++ smart-meter/smart-meter.ino
@@ -85,6 +85,48 @@ void loop()
 
   if (timeElapsed >= 5000) { // every 5 seconds
     lastMessage = millis();
+
+    // calculate CT current
+
+    // 50Hz = 0.02s per peak = 20ms per cycle
+    // In total capture 5 cycles not to slow down the whole system too much (100ms)
+    // emonlib.h use 1480 samples with no delay
+    // Delay = 100 ms / sample size
+    // For 1000 sample size, Delay = 100 uS
+    // For 2000 sample size, Delay = 50 uS
+    // On measurement the analogRead function takes a chunk of time so will try sampling with no delay
+    // Gives ~80-200 ms sample window with 2000 samples which is enough!
+    
+    int samples = 2000;
+    int min = 1023;
+    int max = 0;  
+    for (int i = 0; i<samples; i++) {
+      // offset reading
+      int sensor = analogRead(ct) - (1024/2);
+      min = (sensor < min) ? sensor : min;
+      max = (sensor > max) ? sensor : max;    
+      //delayMicroseconds(50);
+    }
+  
+    // max reading is 1023 @ 3.3V
+    double voltage = 3.3 * ( (float(max-min) / 1024.0) );
+    
+    // 100A:50mA with 22 ohm burden resistor
+    double current = (100.0/3.11) * voltage;
+    double power_ct = 240.0 * current; // assume 240V - RMS?
+  
+    Serial.print("Peak-to-peak: ");
+    Serial.println(max - min);
+    
+    Serial.print(voltage*1000.0);
+    Serial.println(" mV"); 
+  
+    Serial.print(current,3);
+    Serial.println(" A");
+  
+    Serial.print(power_ct,3);
+    Serial.println(" W"); 
+    
     // connect to MQTT server
     if (!client.connected()) {
       Serial.print("Attempting MQTT connection...");
@@ -99,13 +141,14 @@ void loop()
       } //end if client.connect
     } // end if client.connected
     Serial.println("Publishing to MQTT...");    
-     // publish new reading
+    // publish new reading
     client.publish(power_topic, String(power/1000.0).c_str(), true);
     client.publish(energy_topic, String(elapsedkWh).c_str(), true);
   } else {
     delay(100);
     // Serial.println("Not time to publish yet, waiting");
-  } // end if it's time to publish
+  } // end if it's time to publish 
+
 }
 
 // The interrupt routine
