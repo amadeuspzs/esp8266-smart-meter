d18f38a0ad05a9b89e04499c668b75a136e0af68
amadeuspzs <amadeus.stevenson@gmail.com>
2020-09-19 19:33:50 +0100

    Update config.h

--- smart-meter/config.h
+++ smart-meter/config.h
@@ -8,10 +8,7 @@
 // Sensor configuration
 #define optical D1
 #define ppwh 1/3.2 //3200 pulses/kWh = 1/3.2 pulse per Wh
-#define ct A0
-#define samples 1000 // heuristically no improvement above 1000
 
 // Reporting
 #define mqtt_server "0.0.0.0"
-#define power_topic device "/power/optical"
-#define power_ct_topic device "/power/ct"
+#define power_topic device "/power"
