b411d9df6875bce5c2ece498f72e1d41e2befd61
amadeuspzs <amadeus.stevenson@gmail.com>
2020-10-17 19:19:24 +0100

    Final config

--- smart-meter/config.h
+++ smart-meter/config.h
@@ -1,13 +1,14 @@
-#define device "smart-meter"
+#define device "device"
 
 // WiFi credentials.
 #define wifi_hostname device
 #define wifi_ssid "SSID"
-#define wifi_password "PASSPHRASE"
+#define wifi_password "PASSWORD"
 
 // Sensor configuration
 #define optical D1
 #define ppwh 1/3.2 //3200 pulses/kWh = 1/3.2 pulse per Wh
+#define maxpower 24000 // max power (W) you should see, for low pass filtering
 
 // Reporting
 #define mqtt_server "0.0.0.0"
